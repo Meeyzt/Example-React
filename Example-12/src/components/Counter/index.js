c37c0d7f7cf17782be96c2d2c8b4c4e600ee1f78
Mehmet Yılmaz <64976979+Meeyzt@users.noreply.github.com>
2021-07-27 18:43:26 +0300

    Example-12-Test

--- Example-12/src/components/Counter/index.js
+++ Example-12/src/components/Counter/index.js
@@ -1,7 +1,14 @@
-import React from "react";
+import { useState } from "react";
 
 function Counter() {
-  return <div></div>;
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={() => setCount(count + 1)}>Increase</button>
+      <button onClick={() => setCount(count - 1)}>Decrease</button>
+    </div>
+  );
 }
 
 export default Counter;
